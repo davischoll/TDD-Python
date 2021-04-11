1b5be088bad9555b6f96748cd07a8069187bea94
Davi Scholl <davis@bandavide.com.br>
2021-04-10 21:21:03 -0300

    Refatorando Happynumbers

--- happynumbers.py
+++ happynumbers.py
@@ -32,6 +32,33 @@ $2^2 + 0^2 = 4 + 0 = 4
 """
 
 
+def sum_of_squares(num):
+    string = str(num)
+    digits = [int(char) ** 2 for char in string]
+
+    return sum(digits)
+
+
+def happy(num):
+    box = []
+    n = num
+    while n != 1 and n not in box:
+        box.append(n)
+        n = sum_of_squares(n)
+    return n == 1
+
+
+assert sum_of_squares(130) == 10
+assert happy(1)
+assert happy(10)
+assert happy(97)
+assert happy(100)
+assert happy(130)
+assert not happy(4)
+
+
+"""  SEGUNDA SOLUÇÃO, TRANSFORMANDO EM CÓDIGO RECURSIVO:
+
 def happy(num):
     next_ = sum(int(char) ** 2 for char in str(num))
     return num in (1, 7) if num < 10 else happy(next_)
@@ -39,3 +66,5 @@ def happy(num):
 
 assert all(happy(n) for n in (1, 10, 100, 130, 97))
 assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
+
+"""
